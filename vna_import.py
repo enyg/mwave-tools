518e96fb0906c4eddbc708df8bce1a17f14b2bb0
Erich Nygaard <erich.nygaard@gmail.com>
2020-06-11 10:57:40 -0600

    handle s2p files

--- vna_import.py
+++ vna_import.py
@@ -26,3 +26,43 @@ def loadcsv(f):
 						
 		freq = np.array(freq)/1e6
 		return freq, SP
+
+# reads s2p files that are in real/imag format
+def loads2p(f):
+	freq_prefix = 1
+	
+	with open(f, 'r', encoding='utf-8') as s2pfile:
+		s2pReader = csv.reader(s2pfile, delimiter=' ', skipinitialspace=True)
+		for row in s2pReader:
+			if len(row) > 0:
+				if row[0] == '#':
+					# header row
+					if row[1] == 'GHz':
+						freq_prefix = 1e9
+					if row[1] == 'MHz':
+						freq_prefix = 1e6
+					if row[1] == 'kHz' or row[1] == 'KHz':
+						freq_prefix = 1e3
+					assert(row[3] == 'RI')
+					break
+		
+		# determine number of ports (columns)
+		for row in s2pReader:
+			if row[1] == 'Freq':
+				cols = len(row)-1
+				break
+		nports = int((cols-1)/2)	
+		
+		freq = []
+		SP = np.zeros([0,nports])
+		
+		for row in s2pReader:
+			if len(row) == cols:
+				freq.append(float(row[0]))
+				sp = np.zeros([1,nports], dtype=complex)
+				for c in range(nports):
+					sp[0,c] = float(row[1+c*2]) + 1j*float(row[2+c*2])
+				SP = np.concatenate((SP, sp))
+		
+		freq = np.array(freq)*freq_prefix/1e6
+		return freq, SP
